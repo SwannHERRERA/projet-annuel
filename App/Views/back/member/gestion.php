9d249a195700e3859d4fd466b983bf6802104339
Noù <nlarrieulacoste@myges.fr>
2019-05-29 17:10:36 +0200

    BACK filtrage membres

--- App/Views/back/member/gestion.php
+++ App/Views/back/member/gestion.php
@@ -1,38 +1,48 @@
 <div class="mt-20 col-12">
-  <table class="table table-bordered table-striped mb-20">
-    <thead>
-      <tr>
-        <th class='text-center'>Email</th>
-        <th class='text-center'>Pseudo</th>
-        <th class='text-center'>Date</th>
-        <th class='text-center'>Role</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach ($members as $member) :?>
-      <tr>
-          <td><?= $member["email"]?><span class="text-right"><i></i></span></td>
-          <td><?= $member['pseudo']?></td>
-          <td>
-              <?php $date = new DateTime($member['date_inscription']);
-              echo $date->format('d-m-Y'); ?>
-          </td>
-          <td>
-              <?php echo $member['account_role']?><span class="d-inline-block float-right"><a><i class="fas fa-edit"></i></a></span>
-          </td>
-          <td class="text-center">
-              <a href="<?= $site_url . '/back/member/modification?pseudo=' . $member['pseudo']?>"><i class="fas fa-edit"></i></a>
-              <a onclick="confdel('<?= $site_url . '/back/member/delete?pseudo=' . $member['pseudo']?>')" class="text-danger"><i class="fas fa-trash-alt"></i></a>
-              <button class="open-modal text-danger" data-toggle="modal" id="<?= $member['pseudo']?>" data-target="#modal_ban"><i class="fas fa-ban"></i></button>
-          </td>
-      </tr>
-      <?php endforeach; ?>
-    </tbody>
-  </table>
-  <p class="mt-5 mr-5 text-right">
-    <a href="<?= $site_url . '/export.php'?>" class="btn btn-primary">exporter en CSV</a>
-  </p>
+    <div class="row mt-20 mb-20">
+        <div class="col-md-4">
+            <label for="filterMember">Rechercher</label>
+            <input type="text" class="form-control" id="filterMember" onkeyup="filter()">
+        </div>
+    </div>
+    <table class="table table-bordered table-striped mb-20">
+        <thead>
+        <tr>
+            <th class='text-center'>Email</th>
+            <th class='text-center'>Pseudo</th>
+            <th class='text-center'>Date</th>
+            <th class='text-center'>Role</th>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($members as $member) : ?>
+            <tr>
+                <td><?= $member["email"] ?><span class="text-right"><i></i></span></td>
+                <td><?= $member['pseudo'] ?></td>
+                <td>
+                    <?php $date = new DateTime($member['date_inscription']);
+                    echo $date->format('d-m-Y'); ?>
+                </td>
+                <td>
+                    <?php echo $member['account_role'] ?><span class="d-inline-block float-right"><a><i
+                                    class="fas fa-edit"></i></a></span>
+                </td>
+                <td class="text-center">
+                    <a href="<?= $site_url . '/back/member/modification?pseudo=' . $member['pseudo'] ?>"><i
+                                class="fas fa-edit"></i></a>
+                    <a onclick="confdel('<?= $site_url . '/back/member/delete?pseudo=' . $member['pseudo'] ?>')"
+                       class="text-danger"><i class="fas fa-trash-alt"></i></a>
+                    <button class="open-modal text-danger" data-toggle="modal" id="<?= $member['pseudo'] ?>"
+                            data-target="#modal_ban"><i class="fas fa-ban"></i></button>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+    <p class="mt-5 mr-5 text-right">
+        <a href="<?= $site_url . '/export.php' ?>" class="btn btn-primary">exporter en CSV</a>
+    </p>
 </div>
 
 
@@ -47,48 +57,52 @@
             </div>
             <div class="modal-body">
                 <div class="d-flex mb-20">
-                      <div class="mr-auto" id="email_modal"></div>
-                      <div id="pseudo_modal"></div>
+                    <div class="mr-auto" id="email_modal"></div>
+                    <div id="pseudo_modal"></div>
                 </div>
-                <form method="POST" action="<?= $site_url . '/back/member/banMember'?>">
-                <div class="row">
-                    <div class="col-6">
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" value="permanent" checked name="type" id="permanent">
-                            <label class="form-check-label" for="permanent">
-                            Permanent
-                            </label>
+                <form method="POST" action="<?= $site_url . '/back/member/banMember' ?>">
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" value="permanent" checked name="type"
+                                       id="permanent">
+                                <label class="form-check-label" for="permanent">
+                                    Permanent
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" value="temporaire" name="type"
+                                       id="temporaire">
+                                <label class="form-check-label" for="temporaire">
+                                    Temporaire
+                                </label>
+                            </div>
                         </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" value="temporaire" name="type" id="temporaire">
-                            <label class="form-check-label" for="temporaire">
-                            Temporaire
-                            </label>
+                        <div class="col-6 align-self-end">
+                            <select class="select-day form-control form-control-sm" name="nb_day" id="inputState">
+                                <option selected>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                                <option>6</option>
+                                <option>7</option>
+                                <option>8</option>
+                                <option>15</option>
+                            </select>
                         </div>
                     </div>
-                    <div class="col-6 align-self-end">
-                        <select class="select-day form-control form-control-sm" name="nb_day" id="inputState">
-                            <option selected>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                            <option>6</option>
-                            <option>7</option>
-                            <option>8</option>
-                            <option>15</option>
-                        </select>
+                    <input type="hidden" name="email_hidden" id="email_hidden">
+                    <div class="form-group mt-20">
+                        <textarea class="form-control" name="raison" row="3"></textarea>
                     </div>
-                </div>
-                <input type="hidden" name="email_hidden" id="email_hidden">
-                <div class="form-group mt-20">
-                    <textarea class="form-control" name="raison" row="3"></textarea>
-                </div>
-                <p class="text-center"><button type="submit" class="btn btn-primary">Valider</button></p>
+                    <p class="text-center">
+                        <button type="submit" class="btn btn-primary">Valider</button>
+                    </p>
             </div>
         </div>
     </div>
-  </div>
+</div>
 </div>
 
 <script src="<?= $site_url . '/js/ajax_modal_back.js' ?>"></script>
--- public/js/ajax_modal_back.js
+++ public/js/ajax_modal_back.js
@@ -3,14 +3,15 @@ const email_modal = document.querySelector('#email_modal');
 const pseudo_modal = document.querySelector('#pseudo_modal');
 const email_hidden = document.querySelector('#email_hidden');
 
-for (let i = 0; i < btn_modal.length; i++){
-    btn_modal[i].addEventListener('click', function(a){
+for (let i = 0; i < btn_modal.length; i++) {
+    btn_modal[i].addEventListener('click', function (a) {
         a.preventDefault();
         const pseudo = this.id;
         pseudo_modal.innerHTML = pseudo;
         makeRequest(pseudo);
     })
 }
+
 function makeRequest(pseudo) {
     var httpRequest = false;
     httpRequest = new XMLHttpRequest();
@@ -19,8 +20,8 @@ function makeRequest(pseudo) {
         return false;
     }
     httpRequest.open('POST', "https://flixadvisor.fr/back/member/ajaxban", true);
-    httpRequest.onreadystatechange = function() {
-        if(this.readyState === 4) {
+    httpRequest.onreadystatechange = function () {
+        if (this.readyState === 4) {
             let email = JSON.parse(httpRequest.responseText)[0].email;
             email_modal.innerHTML = email;
             email_hidden.value = email;
@@ -30,4 +31,18 @@ function makeRequest(pseudo) {
     httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     httpRequest.send('pseudo=' + encodeURIComponent(pseudo));
 
+}
+
+function filter() {
+    const filter = document.getElementById("filterMember").value.toUpperCase();
+    const table = document.getElementsByTagName("table")[0];
+    const tr = table.getElementsByTagName("tr");
+    for (let i = 1; i < tr.length; i++) {
+        const td = tr[i].getElementsByTagName("td");
+        if (td[0].innerHTML.split("<span")[0].toUpperCase().indexOf(filter) > -1 || td[1].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+        } else {
+            tr[i].style.display = "none";
+        }
+    }
 }
