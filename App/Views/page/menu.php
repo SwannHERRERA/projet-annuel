8cf07981fc0b151b03c766fe0c2f33589c85df9d
Swann HERRERA <wilk65537@gmail.com>
2019-05-31 21:48:08 +0200

    menu mobile

--- App/Views/page/menu.php
+++ App/Views/page/menu.php
@@ -12,6 +12,14 @@
     integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= $site_url . '/css/style.css' ?>">
     <title><?= $page_title ?? "Flix Advisor" ?></title>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous" defer></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous" defer></script>
+    <script src="<?= $site_url . "/js/script.js"?>" defer></script>
 </head>
 
 <body>
@@ -19,27 +27,28 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
+                    <p class="text-center mt-40"><a href="<?= $site_url . '/'?>" class="btn btn-tertiary">Home</a></p>
                     <?php if (!$this->member_model->isConnected()):?>
-                        <p class="text-center mt-40"><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal">
+                        <p class="text-center mt-20"><button type="button" class="btn btn-tertiary" data-toggle="modal" data-target="#modal">
                             Se connecter
                         </button></p>
-                        <p class="text-center"><a href="<?= $site_url . '/member/register'?>" class="btn btn-primary">S'inscrire</a></p>
+                        <p class="text-center"><a href="<?= $site_url . '/member/register'?>" class="btn btn-tertiary">S'inscrire</a></p>
                     <?php else : ?>
-                        <p class="text-center mt-40"><a href="<?= $site_url . '/member/logout'?>" class="btn btn-primary">Déconnexion</a></p>
+                        <p class="text-center mt-40"><a href="<?= $site_url . '/member/logout'?>" class="btn btn-tertiary">Déconnexion</a></p>
                     <?php endif ?>
                     <ul class="mb-20">
-                        <li><a href="#" class="link"><i class="fas fa-search"></i>&nbsp;Recherche avancée</a>
-                        <li><a href="#" class="link"><i class="fas fa-flask"></i>&nbsp;Surprenez-moi !</a>
-                        <li><a href="#" class="link"><i class="fas fa-plus"></i>&nbsp;Proposer une série</a>
-                        <li><a href="#" class="link"><i class="fas fa-address-book"></i>&nbsp;Contact</a>
+                            <li><a href="<?= $site_url . '/recherche_avancee' ?>" class="link text-dark"><i class="fas fa-search"></i>&nbsp;Recherche avancée</a>
+                            <li><a href="#" class="link text-dark"><i class="fas fa-address-book"></i>&nbsp;Contact</a>
                     </ul>
                     <?php if ($this->member_model->isConnected()):?>
                         <p class="text-center">
-                            <a href="#" class="btn btn-secondary"><i class="fas fa-cog"></i></a>
-                            <a href="#" class="btn btn-secondary"><i class="fas fa-question"></i></a>
-                            <a href="#" class="btn btn-secondary"><i class="fas fa-tv"></i></a>
+                                <a href="<?= $site_url . '/member/parameters' ?>" class="btn btn-secondary"><i class="fas fa-cog"></i></a>
                         </p>
                     <?php endif;?>
+
+                    <?php if($this->member_model->is_admin()): ?>
+                        <p class="text-center"><a class="btn btn-success" href="<?= $site_url . '/back/member/gestion'?>">Back office</a></p>
+                    <?php endif ?>
                 </div>
             </div>
         </div>
