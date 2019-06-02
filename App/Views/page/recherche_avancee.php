600ad22be1a104e5883fbcf4e583396f7d7a67f1
Noù <nlarrieulacoste@myges.fr>
2019-06-02 16:30:24 +0200

    Design

--- App/Views/page/recherche_avancee.php
+++ App/Views/page/recherche_avancee.php
@@ -3,13 +3,74 @@
         <div class="col-12">
             <form class="mt-30">
                 <!--<input type="hidden" name="token_csrf" value="<?php //echo $token ?>"> -->
-                <div class="form-group">
-                    <label for="advanced_search">Nom</label>
-                    <input type="text" name="search" id="advanced_search" class="form-control" value="">
-                </div>
                 <div class="row mb-20">
-                    <div class="col-4">
-                        <div class="form-group">
+                    <div class="col-md-6">
+                        <label for="advanced_search".<b>Nom</b></label>
+                        <input type="text" name="search" id="advanced_search" class="form-control" value="">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="search_actor"><b>Acteur / Actrice</b></label>
+                        <input type="text" id="search_actor" class="form-control">
+                        <div id="ajax_result"></div>
+                        <div id="valid_result"></div>
+                    </div>
+                </div>
+
+                <div class="row mb-20">
+                    <div class="col-md-6">
+                        <button type="button" id="runtime" class="btn btn-block btn-primary">Durée en minutes</button>
+                        <div id="time_pickers" style="display: none;">
+                            <? for ($i = 0; $i <= 120; $i += 5): ?>
+                                <input type="checkbox" id="runtime<?= $i ?>" value="<?= $i ?>" name="runtimes[]" style="display: none;">
+                                <label for="runtime<?= $i ?>">
+                                    <div>
+                                        <?= $i ?>
+                                    </div>
+                                </label>
+                            <? endfor ?>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <button type="button" id="first_aired_years" class="btn btn-block btn-primary">Année de première diffusion</button>
+                        <div id="years_pickers" style="display: none;">
+                            <? for ($i = 1970; $i < 2019; $i++): ?>
+                                <input type="checkbox" id="year<?= $i ?>" value="<?= $i ?>" name="years[]" style="display: none;">
+                                <label for="year<?= $i ?>">
+                                    <div>
+                                        <?= $i ?>
+                                    </div>
+                                </label>
+                            <? endfor ?>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row mb-20">
+                    <div class="col-md-6">
+                        <div id="network" class="dropdown-check-list" tabindex="100">
+                            <span class="anchor">Chaîne</span>
+                            <ul class="items">
+                                <?php foreach ($networks as $network) : ?>
+                                    <li><input type="checkbox" value="<?= $network['id_network'] ?>" id="<?= $network['name_network'] ?>" name="networks[]"/><label for="<?= $network['name_network'] ?>">&nbsp;<?= $network['name_network'] ?></label></li>
+                                <?php endforeach ?>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div id="list1" class="dropdown-check-list mb-20" tabindex="100">
+                            <span class="anchor">Genre</span>
+                            <ul class="items">
+                                <?php foreach ($genders as $gender) : ?>
+                                    <li><input type="checkbox" value="<?= $gender['id_category'] ?>" id="<?= $gender['name_category'] ?>" name="gender[]"/><label for="<?= $gender['name_category'] ?>">&nbsp;<?= $gender['name_category'] ?></label></li>
+                                <?php endforeach ?>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row mb-20">
+                    <div class="col-md-6">
                             <label for="minimum_rating">Note minimale</label>
                             <select class="form-control" name="minimum_rating" id="minimum_rating">
                                 <option></option>
@@ -24,42 +85,20 @@
                                 <option>9</option>
                                 <option>10</option>
                             </select>
-                        </div>
-                        <div class="form-group">
-                            <button type="button" id="runtime" class="btn btn-block btn-primary">Durée (en minutes)</button>
-                            <div id="time_pickers" style="display: none;">
-                                <? for ($i = 0; $i <= 120; $i += 5): ?>
-                                <input type="checkbox" id="runtime<?= $i ?>" value="<?= $i ?>" name="runtimes[]" style="display: none;">
-                                <label for="runtime<?= $i ?>">
-                                    <div>
-                                        <?= $i ?>
-                                    </div>
-                                </label>
-                                <? endfor ?>
-                            </div>
-                        </div>
-                        <div id="network" class="dropdown-check-list" tabindex="100">
-                            <span class="anchor">Chaîne</span>
-                            <ul class="items">
-                                <?php foreach ($networks as $network) : ?>
-                                    <li><input type="checkbox" value="<?= $network['id_network'] ?>" id="<?= $network['name_network'] ?>" name="networks[]"/><label for="<?= $network['name_network'] ?>">&nbsp;<?= $network['name_network'] ?></label></li>
-                                <?php endforeach ?>
-                            </ul>
-                        </div>
                     </div>
-                    <div class="col-4">
+                    <div class="col-md-6">
                         <div class="white-card text-dark mb-20 p-10">
                             <span>Statut</span>
                             <div class="form-check">
                                 <input class="form-check-input" type="radio" id="continuing" name="status" value="continuing">
                                 <label class="form-check-label" for="continuing">
-                                    En cour
+                                    En cours
                                 </label>
                             </div>
                             <div class="form-check">
                                 <input class="form-check-input" type="radio" id="ended" name="status" value="ended">
                                 <label class="form-check-label" for="ended">
-                                    Terminé
+                                    Terminée
                                 </label>
                             </div>
                             <div class="form-check">
@@ -69,32 +108,10 @@
                                 </label>
                             </div>
                         </div>
-                        <div id="list1" class="dropdown-check-list mb-20" tabindex="100">
-                            <span class="anchor">Genre</span>
-                            <ul class="items">
-                                <?php foreach ($genders as $gender) : ?>
-                                    <li><input type="checkbox" value="<?= $gender['id_category'] ?>" id="<?= $gender['name_category'] ?>" name="gender[]"/><label for="<?= $gender['name_category'] ?>">&nbsp;<?= $gender['name_category'] ?></label></li>
-                                <?php endforeach ?>
-                            </ul>
-                        </div>
+
                     </div>
-                    <div class="col-4">
-                            <button type="button" id="first_aired_years" class="btn btn-block btn-primary">Année de première diffusion</button>
-                            <div id="years_pickers" style="display: none;">
-                                <? for ($i = 1999; $i < 2019; $i++): ?>
-                                <input type="checkbox" id="year<?= $i ?>" value="<?= $i ?>" name="years[]" style="display: none;">
-                                <label for="year<?= $i ?>">
-                                    <div>
-                                        <?= $i ?>
-                                    </div>
-                                </label>
-                                <? endfor ?>
-                            </div>
-                        <div id="valid_result"></div>
-                        <label for="search_actor">Acteur / Actrice</label>
-                        <input type="text" id="search_actor" class="form-control">
-                        <div id="ajax_result"></div>
-                    </div>
+                </div>
+
                     <div class="col-12">
                         <p class="text-center">
                             <button type="submit" class="btn btn-gold" name="">Chercher</button>
@@ -103,36 +120,54 @@
             </form>
         </div>
         <div class="row">
-            <div class="col-12">
+
                 <?php if (!empty($tv_shows)): ?>
                 <?php foreach ($tv_shows as $tv_show) : ?>
-                    <div class="card mb-30">
+            <div class="col-md-6">
+                    <div class="card mb-30" style="margin-left: 15px; margin-right: 15px">
                         <div class="card-content">
                             <div class="card-header">
                                 <a href="<?= $site_url . '/show?show=' . $tv_show['id_show']?>"><h5 class="card-title"><?= $tv_show['name_show'] ?></h5></a>
                             </div>
                             <div class="card-body">
                                 <div class="row">
-                                    <div class="col-md-6">
+                                    <div class="col-xl-6">
                                         <div class="text-center">
-                                            <a href="<?= $site_url . '/show?show=' . $tv_show['id_show']?>"><img src="<?= $tv_show['image_show'] ?>" class="img-fluid" alt="banniere serie <?= $tv_show['name_show'] ?>"></a>
+                                            <a href="<?= $site_url . '/show?show=' . $tv_show['id_show']?>"><img src="<?= $tv_show['image_show'] ?>" class="img-fluid" alt="Image série <?= $tv_show['name_show'] ?>"></a>
                                         </div>
                                     </div>
-                                    <div class="col-md-6">
-                                        <p class="card-text"><?= $tv_show['summary_show'] ?></p>
+                                    <div class="col-xl-6">
+                                        <p id="summarySearch" class="card-text" style="text-align: justify">
+                                            <?= $tv_show['summary_show'] ?>
+                                        </p>
                                     </div>
                                 </div>
                             </div>
                             <ul class="list-group list-group-flush">
-                                <li class="list-group-item"><?= $tv_show['production_status'] == 'ended' ? 'Terminé' : 'En cour' ?></li>
-                                <li class="list-group-item"><?= $tv_show['followers'] ?> Abonnées</li>
-                                <li class="list-group-item">Durée moyenne d'un épisode <?= $tv_show['runtime_show'] ?> minutes</li>
+                                <li class="list-group-item">Statut de production : <?= $tv_show['production_status'] == 'ended' ? 'terminé' : 'en cours' ?></li>
+                                <?php
+                                    $nbFollowers = $tv_show['followers'];
+                                    if ($nbFollowers == 0) {
+                                        $nbFollowers = "Aucun";
+                                    }
+                                    $word = $nbFollowers > 1 ? 'membres' : 'membre';
+                                    if ($nbFollowers > 1) {
+                                        $verb = 'suivent';
+                                    } else if ($nbFollowers == 1) {
+                                        $verb = 'suit';
+                                    } else {
+                                        $verb = 'ne suit';
+                                    }
+                                ?>
+                                <li class="list-group-item"><?= $nbFollowers." ".$word. " ".$verb ?> cette série</li>
+                                <li class="list-group-item">Durée moyenne d'un épisode : <?= $tv_show['runtime_show'] ?> minutes</li>
                             </ul>
                         </div>
                     </div>
+            </div>
                 <?php endforeach; ?>
             <?php endif ?>
-            </div>
+
         </div>
     </div>
 </div>
