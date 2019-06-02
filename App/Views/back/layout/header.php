24432af072fb8319a3e729745a36dfacb877a9ce
Noù <nlarrieulacoste@myges.fr>
2019-06-02 17:03:37 +0200

    Update header.php

--- App/Views/back/layout/header.php
+++ App/Views/back/layout/header.php
@@ -6,47 +6,47 @@ $site_url = (isset($_SERVER['HTTPS'])) ? 'https://' : 'http://';
 $site_url .= $_SERVER['HTTP_HOST'];
 $uri_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri_segments = explode('/', $uri_path);
-$categories = ['member' => 'Membre', 'tv_show' => 'Series','ticket' => 'Contact', 'statistics' => 'Statistique'];
+$categories = ['member' => 'Membre', 'tv_show' => 'Series', 'ticket' => 'Contact', 'statistics' => 'Statistique'];
 ?>
 <head>
-  <meta charset="UTF-8">
-  <title><?= $page_title ?? 'back_flixAdvisors'?></title>
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-  <link rel="stylesheet" href="<?= $site_url . '/css/style_back.css'?>">
+    <meta charset="UTF-8">
+    <title><?= $page_title ?? 'back_flixAdvisors' ?></title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= $site_url . '/css/style_back.css' ?>">
 </head>
 
 <body class='containter-fluid'>
-  <div class='row no-gutters vh-100'>
+<div class='row no-gutters vh-100'>
     <aside class='col-md-2'>
-      <ul class='pt-20 mb-30'>
-        <?php foreach ($categories as $controller => $categorie) :?>
-        <li class='text-center'><a href="<?= $site_url . "/back/" . $controller?>"
-            class="<?php if ($uri_segments[2] == $controller) echo "active"; ?>"><?= $categorie?></a>
-        </li>
-        <?php endforeach;?>
-      </ul>
-      <p class="text-center"><a class="btn btn-danger" href="/">Front</a></p>
+        <ul class='pt-20 mb-30'>
+            <?php foreach ($categories as $controller => $categorie) : ?>
+                <li class='text-center'><a href="<?= $site_url . "/back/" . $controller ?>"
+                                           class="<?php if ($uri_segments[2] == $controller) echo "active"; ?>"><?= $categorie ?></a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+        <p class="text-center"><a class="btn btn-danger" href="/">Front</a></p>
     </aside>
     <div class="col-md-10">
-      <main class="container">
-        <div class="row">
-          <div class="col-12">
-            <h1 class="pt-30 pb-20"><?= $page_title; ?></h1>
-            <hr>
-            <?= '<ul class="onglet">'?>
-              <?php
-              if (isset($sous_categories)) {
-              foreach ($sous_categories as $sous_categorie => $segment) :
-                  echo '<li class="';
-                  echo ($uri_segments[3] == $segment || $uri_segments[3] == $categorie_active) ? 'active' : '';?>"<?php echo '>';
-                  echo '<a href="';
-                  echo $site_url . '/back/' . $uri_segments[2] . '/' . $segment ;
-                  echo '">';
-                  echo $sous_categorie;
-                  echo '</a>';
-                  echo '</li>';
-              endforeach;
-              echo '</ul>';?>
-              }
-          </div>
+        <main class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="pt-30 pb-20"><?= $page_title; ?></h1>
+                    <hr>
+                    <?= '<ul class="onglet">' ?>
+                    <?php
+                    if (isset($sous_categories)) {
+                        foreach ($sous_categories as $sous_categorie => $segment) :
+                            echo '<li class="';
+                            echo ($uri_segments[3] == $segment || $uri_segments[3] == $categorie_active) ? 'active' : ''; ?>"<?php echo '>';
+                            echo '<a href="';
+                            echo $site_url . '/back/' . $uri_segments[2] . '/' . $segment;
+                            echo '">';
+                            echo $sous_categorie;
+                            echo '</a>';
+                            echo '</li>';
+                        endforeach;
+                        echo '</ul>';
+                    } ?>
+                </div>
