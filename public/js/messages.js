398050ad56aea5cb5b109307b49d1aadeb91fbd6
Noù <nlarrieulacoste@myges.fr>
2019-06-02 17:11:13 +0200

    js fix

--- public/js/messages.js
+++ public/js/messages.js
@@ -15,8 +15,8 @@ function getMessages(pseudo) {
 function sendMessage() {
     const pseudo = document.getElementsByClassName("list-group-item list-group-item-action active")[0].innerHTML;
     const element = document.getElementById("messages");
-    const message = document.getElementById("newMessage").value;
-    if (message.length > 0) {
+    const message = document.getElementById("newMessage");
+    if (message.value.length > 0) {
         const request = new XMLHttpRequest();
         request.onreadystatechange = function () {
             if (request.readyState === 4) {
@@ -30,7 +30,7 @@ function sendMessage() {
         request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         request.send(
             'pseudo=' + pseudo +
-            '&message=' + message
+            '&message=' + message.value
         );
     }
 }
